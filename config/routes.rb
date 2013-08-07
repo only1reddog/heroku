1993273e2393bb10f0ba9cbf172f7ef140455ea1
only1reddog <only1reddog@gmail.com>
2013-08-07 19:08:07 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ SampleApp::Application.routes.draw do
   
   match '/signup', to: 'users#new', via: 'get'
 
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  
   resources :users
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
